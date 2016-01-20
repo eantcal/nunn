faafc46641ff3cb7bb61f2ac08fa57242b2f15ae
Antonino Calderone <antonino.calderone@ericsson.com>
2016-01-20 14:51:00 +0100

    Update legal info

--- ocr_test/ocr_test.cpp
+++ ocr_test/ocr_test.cpp
@@ -1,18 +1,18 @@
 /*
-*  This file is part of nuNN
+*  This file is part of nunnlib
 *
-*  nuNN is free software; you can redistribute it and/or modify
+*  nunnlib is free software; you can redistribute it and/or modify
 *  it under the terms of the GNU General Public License as published by
 *  the Free Software Foundation; either version 2 of the License, or
 *  (at your option) any later version.
 *
-*  nuNN is distributed in the hope that it will be useful,
+*  nunnlib is distributed in the hope that it will be useful,
 *  but WITHOUT ANY WARRANTY; without even the implied warranty of
 *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 *  GNU General Public License for more details.
 *
 *  You should have received a copy of the GNU General Public License
-*  along with nuNN; if not, write to the Free Software
+*  along with nunnlib; if not, write to the Free Software
 *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  US
 *
 *  Author: Antonino Calderone <acaldmail@gmail.com>
@@ -56,7 +56,7 @@
 #define WHITEBOARD_Y 130
 #define YBMPOFF 100
 
-#define FILE_FILTER "nuNN (.net)\0*.net;\0All Files (*.*)\0*.*\0\0";
+#define FILE_FILTER "nunnlib (.net)\0*.net;\0All Files (*.*)\0*.*\0\0";
 
 
 
